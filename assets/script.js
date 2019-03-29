50b4068d86f84a7f45581df4cb7f6e526fccdf45
Lnola <nola.luka@gmail.com>
2019-03-29 01:18:26 +0100

    Basics started

---
+++ assets/script.js
@@ -0,0 +1,73 @@
+if (localStorage.getItem("Username") !== null) {
+    switchSignupToLogin();
+}
+
+let signUpSubmitButton = document.getElementById("s-submit");
+
+signUpSubmitButton.addEventListener("click", function () {
+    let signUpInputsArray = document.getElementsByClassName("s-input");
+
+    if (isAnyInputEmpty(signUpInputsArray)) {
+        if (checkRepeatPassword(signUpInputsArray) && checkMinLetters(signUpInputsArray)) {
+            localStorage.setItem('Username', signUpInputsArray[0].value);
+            localStorage.setItem('Password', signUpInputsArray[1].value);
+            alert("User saved");
+        }
+    }
+});
+
+let signInSubmitButton = document.getElementById("l-submit");
+
+signInSubmitButton.addEventListener("click", function () {
+    let signInInputsArray = document.getElementsByClassName("l-input");
+
+    if (isAnyInputEmpty(signInInputsArray) && isSameAsLocalStorage(signInInputsArray)) {
+        switchLoginToUsers();
+    }
+});
+
+function switchSignupToLogin() {
+    $("#signup-input").addClass("display-none");
+    $("#login-input").removeClass("display-none");
+}
+
+function switchLoginToUsers() {
+    $("#login-input").addClass("display-none");
+    $("#test").removeClass("display-none");
+}
+
+function isAnyInputEmpty(array) {
+    for (let input of array) {
+        if (input.value === "") {
+            alert("All inputs are required");
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkRepeatPassword(array) {
+    if (array[1].value !== array[2].value) {
+        alert("Passwords don't match");
+        return false;
+    }
+    return true;
+}
+
+function checkMinLetters(array) {
+    for (let input of array) {
+        if (input.value.length < 5) {
+            alert("Input is too short");
+            return false;
+        }
+    }
+    return true;
+}
+
+function isSameAsLocalStorage(array) {
+    if (array[0].value === localStorage.getItem("Username") && array[1].value === localStorage.getItem("Password"))
+        return true;
+
+    alert("Username or password don't match")
+    return false;
+}
